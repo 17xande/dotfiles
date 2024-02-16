a2e1e51977843e55d97fdc9f1fe73663c6aa5ccc
Alexandre Figueiredo <17xande@gmail.com>
2024-02-16 14:48:40 +0200

    add terraform

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -334,7 +334,7 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash', 'markdown' },
+      'bash', 'markdown', 'terraform' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -481,6 +481,7 @@ local servers = {
   html = { filetypes = { 'html', 'twig', 'hbs' } },
   tailwindcss = {},
   htmx = {},
+  terraformls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
